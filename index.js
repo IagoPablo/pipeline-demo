82a4d1aa8a8e3394f31b48508508c8337c7834c7
Iago <iagopablo25@gmail.com>
2025-10-27 21:24:41 -0300

    feat: função de saudação

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Testando GitHub Actions");
+function saudacao(nome) {
+  return `Olá, ${nome}! Seja bem-vindo(a) ao projeto com GitHub Actions.`;
+}
+
+const mensagem = saudacao("Iago");
+console.log(mensagem);
