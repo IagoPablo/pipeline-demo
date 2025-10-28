af769dde2a45d5dc72dd8ce7fd197c5b625b8e49
iagopablo25@gmail.com <iago.silva@cs.unipe.edu.br>
2025-10-28 08:06:52 -0300

    Iago pablo

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function saudacao(nome) {
   return `Olá, ${nome}! Seja bem-vindo(a) ao projeto com GitHub Actions.`;
 }
 
-const mensagem = saudacao("Iago");
+const mensagem = saudacao("Iago Pablo");
 console.log(mensagem);
